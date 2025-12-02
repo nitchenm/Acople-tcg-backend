b5b575fb09b2d7201b989fe8a922917a61e8cd2c
nitchenm <66035129+nitchenm@users.noreply.github.com>
2025-12-02 15:08:13 -0300

    Update WebConfig.java

--- src/main/java/com/acople/tcg_backend/config/WebConfig.java
+++ src/main/java/com/acople/tcg_backend/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**") // Apply to all endpoints under /api
-                        .allowedOrigins("https://acople-shop-tcg.vercel.app/") // Your frontend URL
+                        .allowedOrigins("*") // Your frontend URL
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
@@ -21,4 +21,5 @@ public class WebConfig {
         };
     }
 }
+
 
